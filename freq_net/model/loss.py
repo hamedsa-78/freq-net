65aebbe3ada64a764d070032cae717d08ad74084
hamedsa-78 <hamed.saadati078@gmail.com>
2023-08-05 05:11:03 +0330

    hyper_params

--- freq_net/model/loss.py
+++ freq_net/model/loss.py
@@ -1,30 +1,42 @@
 import torch
 
 
-def charbonnier_loss(
-    x: torch.Tensor, y: torch.Tensor, bc: torch.Tensor, epsilon: float = 1e-6
-) -> torch.Tensor:
-    """
-    Computes the Charbonnier loss between two tensors x and y along with the frequency loss.
+class CharbonnierLoss:
+    def __init__(self, epsilon: float = 1e-6):
+        self.bc = self.weight_initialization_R10()
+        self.epsilon = epsilon
 
-    Args:
-        x (torch.Tensor): The predicted tensor of shape (B, C, H, W).
-        y (torch.Tensor): The target tensor of shape (B, C, H, W).
-        bc (torch.Tensor): The weights for each channel of x of shape (C,).
-        epsilon (float, optional): A small value to avoid division by zero. Defaults to 1e-6.
+    @staticmethod
+    def weight_initialization_R10():
+        """
+        weight initialization for R = 10 (10 , 10) feature maps
+        """
+        weights = [1, 1, 1, 1, 5, 10, 10, 5, 1, 1]
+        weight_tensors = torch.ones((10, 10), dtype=torch.float32)
+        for i in range(10):
+            for j in range(10):
+                weight_tensors[i, j] = weights[max(i, j)]
+        return weight_tensors
 
-    Returns:
-        torch.Tensor: The Charbonnier loss along with the frequency loss.
-    """
-    assert x.shape == y.shape, "x and y tensors must have the same shape"
-    assert len(x.shape) == 4, "x and y tensors must have shape of (B, C, H, W)"
-    B, C, H, W = x.shape
+    def __call__(self, x, y):
+        """
+        Computes the Charbonnier loss between two tensors x and y along with the frequency loss.
 
-    assert (
-        len(bc.shape) == 1 and bc.shape[-1] == C
-    ), "Weights tensor must have the shape of (C,)"
+        Args:
+            x (torch.Tensor): The predicted tensor of shape (*, C).
+            y (torch.Tensor): The target tensor same shape as x.
+            bc (torch.Tensor): The weights for each channel of x of shape (C,).
+            epsilon (float, optional): A small value to avoid division by zero. Defaults to 1e-6.
 
-    charbonnier = torch.sqrt((x - y).pow(2) + epsilon) * bc.view(1, -1, 1, 1)
-    freq_loss = torch.sum(charbonnier) / torch.prod(torch.tensor(x.shape))
+        Returns:
+            torch.Tensor: The Charbonnier loss along with the frequency loss.
+        """
+        assert x.shape == y.shape, "x and y tensors must have the same shape"
+        assert (
+            x.shape[-1] == self.bc.shape[-1]
+        ), "x and y tensors must have the same channel dim "
 
-    return freq_loss
+        charbonnier = torch.sqrt((x - y).pow(2) + self.epsilon**2) * self.bc
+        freq_loss = torch.sum(charbonnier) / torch.prod(torch.tensor(x.shape))
+
+        return freq_loss
